3687f000df3628179bffe401051c63ab8cac2fa9
Keshav Bansal <officialkeshavbansal@gmail.com>
2025-07-07 13:39:51 +0530

    JS if and else-if practice

---
+++ assignements.js
@@ -0,0 +1,23 @@
+let size = "L";
+
+//Popcorn price system
+
+if (size == "XL") {
+    console.log("250");
+}
+
+else if (size == "L") {
+    console.log("200");
+}
+
+else if (size == "M") {
+    console.log("100");
+}
+
+else if (size == "S") {
+    console.log("50");
+}
+
+else {
+    console.log("Not Available")
+}
