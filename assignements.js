bf4074a0b671175eacaf1cffe0f0a8b00dfde4c3
Keshav Bansal <officialkeshavbansal@gmail.com>
2025-07-17 13:52:54 +0530

    Assignement: Question 1

--- assignements.js
+++ assignements.js
@@ -1,15 +1,19 @@
-// Create a Function that returns the concatenation of all strings in an array.
+//Write a JavaScript function that returns array elements larger than a number.
 
-strings = ["Hello", "World", "My", "Name", "is", "Keshav"];
+let arr1 = [8,9,1,5,3,2,4,6,2,8,1,7,6,5,9,1,3,4,0,2,5,0];
+let num1 = 3;
 
-function concatStr(strings) {
-    let result = "";
-    
-    for (let str of strings) {
-        result += str;
+function greaterThan(num, array) {
+
+    let greaterThan = [];
+
+    for (n of array) {
+        if (n > num) {
+            greaterThan.push(n);
+        }
     }
 
-    return result
+    return greaterThan;
 }
 
-console.log(concatStr(strings))
+console.log(greaterThan(num1,arr1));
