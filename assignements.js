532b9e9a090c95393d881906015bc14934735b05
Keshav Bansal <officialkeshavbansal@gmail.com>
2025-07-17 19:31:33 +0530

    Practice Question 1

--- assignements.js
+++ assignements.js
@@ -1,12 +1,3 @@
-//Write a JavaScript function to generate a random number within a range(start,end)
+// Write an arrow function that returns the square of a number 'n'.
 
-let start = 1;
-let end = 10;
-
-function randomBetween(start,end) {
-    let diff = end - start;
-    let randomNumber = Math.floor(Math.random()*diff) + start;
-    return randomNumber; 
-};
-
-console.log(randomBetween(start,end));
+let square = (n) => n * n;
