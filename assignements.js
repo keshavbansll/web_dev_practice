cdb3c82bae484b795dba21acd8e079e41f470335
Keshav Bansal <officialkeshavbansal@gmail.com>
2025-07-24 12:06:17 +0530

    Assignement Questions

--- assignements.js
+++ assignements.js
@@ -1,21 +1,40 @@
-let para1 = document.createElement("p");
-para1.innerText = "Hey I'm red!";
-document.querySelector("body").append(para1);
-para1.classList.add("red");
+let body = document.querySelector("body");
 
-let h3 = document.createElement("h3");
-h3.innerText = "Hey I'm a blue h3!";
-document.querySelector("body").append(h3);
-h3.classList.add("blue");
+//Create a new input and button element on the page using JavaScript only. Set the
+//text of button to "Click Me"
 
-let div = document.createElement("div");
-document.querySelector("body").append(div);
-div.classList.add("container");
+let input = document.createElement("input");
+let button = document.createElement("button");
+button.innerText = "Click Me";
 
-let divHeading = document.createElement("h1");
-let divPara = document.createElement("p");
-divHeading.innerText = "I'm in a div";
-divPara.innerText = "Me too!";
+body.append(input);
+body.append(button);
 
-div.append(divHeading);
-div.append(divPara);
+//Add following attributes to the element:
+// - Change placeholder value of input to "username"
+// - Change the id of button to "btn"
+
+input.setAttribute("placeholder", "username");
+button.setAttribute("id", "btn");
+
+//Access the btn using the querySelector and button id. Change the button background
+//Color to blue and text color to white.
+
+button.style.backgroundColor = "blue";
+button.style.color = "white";
+
+//Create an h1 element on the page and set its text to "DOM Practice". underlined.
+//Change its color to purple
+
+let h1 = document.createElement("h1");
+body.append(h1);
+h1.innerText = "DOM Practice";
+h1.style.textDecoration = "underline";
+h1.style.color = "purple";
+
+//Create a p tag on the page and set its text to  "Web Dev JavaScript Practice" where
+//JavaScript is bold.
+
+let p = document.createElement("p");
+p.innerHTML = "Web Dev <b>JavaScript</b> Practice";
+body.append(p);
