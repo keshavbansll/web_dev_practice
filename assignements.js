0a1a4dbb51f8ba377578a07f5ad924faf9bbe94c
Keshav Bansal <officialkeshavbansal@gmail.com>
2025-07-25 11:03:09 +0530

    Random color generator Activity using event listeners

--- assignements.js
+++ assignements.js
@@ -1,40 +1,21 @@
-let body = document.querySelector("body");
+let btn = document.querySelector("button");
+let heading = document.querySelector("h3");
+let div = document.querySelector("div");
 
-//Create a new input and button element on the page using JavaScript only. Set the
-//text of button to "Click Me"
+function getRandomColor() {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
 
-let input = document.createElement("input");
-let button = document.createElement("button");
-button.innerText = "Click Me";
+  let color = `rgb(${r}, ${g}, ${b})`;
+  return color;
+}
 
-body.append(input);
-body.append(button);
+btn.addEventListener("click", function () {
+  let randomColor = getRandomColor();
+  heading.innerText = randomColor;
 
-//Add following attributes to the element:
-// - Change placeholder value of input to "username"
-// - Change the id of button to "btn"
+  div.style.backgroundColor = randomColor;
 
-input.setAttribute("placeholder", "username");
-button.setAttribute("id", "btn");
-
-//Access the btn using the querySelector and button id. Change the button background
-//Color to blue and text color to white.
-
-button.style.backgroundColor = "blue";
-button.style.color = "white";
-
-//Create an h1 element on the page and set its text to "DOM Practice". underlined.
-//Change its color to purple
-
-let h1 = document.createElement("h1");
-body.append(h1);
-h1.innerText = "DOM Practice";
-h1.style.textDecoration = "underline";
-h1.style.color = "purple";
-
-//Create a p tag on the page and set its text to  "Web Dev JavaScript Practice" where
-//JavaScript is bold.
-
-let p = document.createElement("p");
-p.innerHTML = "Web Dev <b>JavaScript</b> Practice";
-body.append(p);
+  console.log("color updated");
+});
